7bae64451efb7fda2d9f541334fb46148fae9a1c
Joyce121 <115353880+Joyce121@users.noreply.github.com>
2024-04-19 09:23:26 +0800

    Update WL_extract_10%.5%.2%.1%.0.5%.sh

--- WL_extract_10%.5%.2%.1%.0.5%.sh
+++ WL_extract_10%.5%.2%.1%.0.5%.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-# 指定文件路径
 file_path="WL.nsl.out.50kb.windows.sort"
-
-# 统计文件行数
 line_count=$(wc -l < "$file_path")
 
-# 计算并输出结果到新文件
-tail -n $(echo "$line_count * 0.005" | bc | awk '{printf "%.0f\n", $1}') "$file_path" > WL.nsl.out.50kb.windows.sort_0.5%
 tail -n $(echo "$line_count * 0.01" | bc | awk '{printf "%.0f\n", $1}') "$file_path" > WL.nsl.out.50kb.windows.sort_1%
-tail -n $(echo "$line_count * 0.02" | bc | awk '{printf "%.0f\n", $1}') "$file_path" > WL.nsl.out.50kb.windows.sort_2%
 tail -n $(echo "$line_count * 0.05" | bc | awk '{printf "%.0f\n", $1}') "$file_path" > WL.nsl.out.50kb.windows.sort_5%
 tail -n $(echo "$line_count * 0.1" | bc | awk '{printf "%.0f\n", $1}') "$file_path" > WL.nsl.out.50kb.windows.sort_10%
 
